bf4154600a557ce38c066e98f24782eda254f0c3
madyoda <tom@yoda.pw>
2016-07-31 16:12:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+UptimeRobot-Java
+################
+
+
+Work in progress.
---
+++ src/main/java/com/madyoda/uptimerobot/UptimeRobot.java
@@ -0,0 +1,11 @@
+package com.madyoda.uptimerobot;
+
+/**
+ * Copyright (C) madyoda // madyoda.com
+ * Created by tom on 7/31/2016.
+ */
+public class UptimeRobot {
+
+
+
+}
